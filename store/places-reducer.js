58471ee7e1a28057d832c1bbae34af25b8a08d73
Maksym Shokin <maksym.sh@upplabs.com>
2020-02-12 12:18:29 +0200

    'fetching-from-database'

--- store/places-reducer.js
+++ store/places-reducer.js
@@ -1,4 +1,4 @@
-import { ADD_PLACE } from './places-actions'
+import { ADD_PLACE, SET_PLACES } from './places-actions'
 import Place from '../models/places';
 
 const initialState = {
@@ -13,11 +13,15 @@ export default (state = initialState, action) => {
         action.placeData.title,
         action.placeData.imageUri
       );
-      console.log(action.placeData)
+
       return {
         places: state.places.concat(newPlace)
       };
       break;
+    case SET_PLACES:
+      return {
+        places: action.places.map(place => new Place(place.id.toString(), place.title, place.imageUri))
+      }
     default:
       return state
   }
